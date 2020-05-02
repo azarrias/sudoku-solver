19e38a5c11c84e9b440be03beb87710fdb57dd3b
azarrias <adolfo.zarrias@gmail.com>
2020-05-02 20:09:26 +0200

    Replace the '.' character with every possible candidate digit for non solved boxes

--- src/solution.py
+++ src/solution.py
@@ -1,7 +1,7 @@
 from utils import *
 
 def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
 
     Args:
         grid: Sudoku grid in string form, 81 characters long
@@ -10,8 +10,10 @@ def grid_values(grid):
         - keys: Box labels, e.g. 'A1'
         - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
     """
-    assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
-    return dict(zip(boxes, grid))
+    all_digits = '123456789'
+    values = [all_digits if c == '.' else c for c in grid]
+    assert len(values) == 81, "Input grid must be a string of length 81 (9x9)"
+    return dict(zip(boxes, values))
 
 if __name__ == "__main__":
     sudoku_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
